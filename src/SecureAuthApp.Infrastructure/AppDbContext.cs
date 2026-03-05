3d7012ca2535956f4eb72d6bfa20dd7f98dee7d8
Luanderson-Dev <contato@luanderson.dev.br>
2026-03-04 22:11:28 -0400

    feat: set User to AppDbContext, and UserRepository

--- src/SecureAuthApp.Infrastructure/AppDbContext.cs
+++ src/SecureAuthApp.Infrastructure/AppDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SecureAuthApp.Domain;
 
 namespace SecureAuthApp.Infrastructure;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
+    public DbSet<User> Users { get; set; }
 }
---
+++ src/SecureAuthApp.Infrastructure/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using SecureAuthApp.Domain;
+
+namespace SecureAuthApp.Infrastructure;
+
+public class UserRepository: IUserRepository
+{
+    private readonly AppDbContext _context;
+    
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public void Add(User user) => _context.Users.Add(user);
+    public User? GetByEmail(string email) => _context.Users.FirstOrDefault(u => u.Email == email);
+    public void SaveChanges() => _context.SaveChanges();
+}
