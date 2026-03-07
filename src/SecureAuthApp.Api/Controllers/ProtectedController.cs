0e663ff78488e2b71744c0453299d0b77d763974
Luanderson-Dev <contato@luanderson.dev.br>
2026-03-06 21:59:20 -0400

    feat: create Protected endpoint for access testing.

---
+++ src/SecureAuthApp.Api/Controllers/ProtectedController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecureAuthApp.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class ProtectedController: ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetProtectedData()
+    {
+        return Ok(new {
+            Message = "This is protected data accessible only to authenticated users. You're authenticated successfully!", 
+            UserEmail = User.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Email)?.Value});
+    }
+}
