ed18450cdb6349326a7216c89aeb2fe36a69eb97
Luanderson-Dev <contato@luanderson.dev.br>
2026-03-06 20:33:54 -0400

    feat: updated User entity, add Role, CreatedAt and methed for password update for later

--- src/SecureAuthApp.Domain/Entities/User.cs
+++ src/SecureAuthApp.Domain/Entities/User.cs
@@ -5,11 +5,22 @@ public class User
     public Guid Id { get; private set; }
     public string Email { get; private set; }
     public string PasswordHash { get; private set; }
+    public string Role { get; private set; }
+    public DateTime CreatedAt { get; private set; }
 
-    public User(string email, string passwordHash)
+    protected User() {}
+
+    public User(string email, string passwordHash, string role = "User")
     {
         Id = Guid.NewGuid();
         Email = email;
         PasswordHash = passwordHash;
+        Role = role;
+        CreatedAt = DateTime.UtcNow;
+    }
+    
+    public void UpdatePassword(string newPasswordHash)
+    {
+        PasswordHash = newPasswordHash;
     }
 }
